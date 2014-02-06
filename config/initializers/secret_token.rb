a1e09b2149da10776e19d7627f49a5f03dd9388d
Zoricus <michaweb@gmx.de>
2014-02-06 23:38:14 +0100

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Statbets::Application.config.secret_key_base = 'e5f1658315a40e3c89f21241c97ec7a27bf671a6647d2a5813f77d00684a685c9d14ebe9843ae9c937806eceedb95714d62b3c4d4a0b308496c5e02d65335f75'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Statbets::Application.config.session_store :cookie_store, key: '_statbets_session'
